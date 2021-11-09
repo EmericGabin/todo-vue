e0aaa9e7d302b1157729443afe236b93203949e5
manudurgoni <emmanuel@carillon.studio>
2021-11-09 16:18:23 +0100

    add animation

--- src/TodoApp.vue
+++ src/TodoApp.vue
@@ -3,11 +3,13 @@
     <button @click="toggleForm" class="fixed bottom-4 right-4 w-14 h-14 rounded-full bg-gray-800 text-white text-xl z-10">{{ labelBtn }}</button>
 
     <transition name="fade">
-      <MonFormulaire v-show="showForm" :tasks="tasks" @submit="toggleForm(false)"></MonFormulaire>
+      <MonFormulaire v-show="showForm" :tasks="tasks" @submit="toggleForm(null, false)"></MonFormulaire>
     </transition>
 
     <div class="tasks overflow-auto">
-      <MaTache v-for="task in tasks" :key="task.id" :task="task" @delete="onDelete"></MaTache>
+      <transition-group name="list" tag="div">
+        <MaTache v-for="task in tasks" :key="task.id" :task="task" @delete="onDelete"></MaTache>
+      </transition-group>
     </div>
   </div>
 </template>
@@ -39,7 +41,8 @@ export default {
       this.tasks.splice(id, 1)
     },
 
-    toggleForm(value) {
+    toggleForm(event, value) {
+      console.log(value)
       if (value !== undefined) {
         this.showForm = value
       } else {
@@ -60,5 +63,14 @@ export default {
 }
 .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
   opacity: 0;
+}
+
+
+.list-enter-active, .list-leave-active {
+  transition: all 1s;
+}
+.list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: translateY(30px);
 }
 </style>
